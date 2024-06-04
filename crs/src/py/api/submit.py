584da30fd77e1ccb45b75b02ffbf717596f3aa9f
Dan Ristea <dristea@turing.ac.uk>
2024-06-04 17:10:16 +0000

    Add submission endpoint

--- crs/src/py/api/submit.py
+++ crs/src/py/api/submit.py
@@ -1,28 +1,35 @@
+import base64
 import time
 from typing import TypedDict, Literal, TypeAlias
 from requests import Session
 from requests.auth import HTTPBasicAuth
 from config import AIXCC_API_HOSTNAME
 
+CPVuuid: TypeAlias = str
+GPuuid: TypeAlias = str
+CAPIStatus: TypeAlias = Literal["accepted", "pending", "rejected"]
+
 
 class ProofOfVulnerability(TypedDict):
-    harness: str
-    data: str
+    harness: str  # harness id from project.yaml e.g. id_1
+    data: str  # input for harness to trigger vulnerability
 
 
 class ProofOfUnderstanding(TypedDict):
-    commit_sha1: str
-    sanitizer: str
+    commit_sha1: str  # git commit hash that introduced vulnerability
+    sanitizer: str  # sanitizer id from project.yaml e.g. id_1
 
 
-class VDSSubmission(TypedDict):
-    cp_name: str
+class VDSubmission(TypedDict):
+    cp_name: str  # directory name of the challenge project NOT the name from project.yaml
     pou: ProofOfUnderstanding
     pov: ProofOfVulnerability
 
 
-VDuuid: TypeAlias = str
-CAPIStatus: TypeAlias = Literal["accepted", "pending", "rejected"]
+class GPSubmission(TypedDict):
+    cpv_uuid: CPVuuid
+    data: str  # patch in unified diff format (www.gnu.org/software/diffutils/manual/html_node/Unified-Format.html)
+
 
 AUTH = HTTPBasicAuth("00000000-0000-0000-0000-000000000000", "secret")
 
@@ -44,30 +51,55 @@ def healthcheck() -> bool:
         return False
 
 
-def submit_vds(vds: VDSSubmission) -> tuple[VDuuid, CAPIStatus]:
+def submit_vulnerability(vds: VDSubmission) -> tuple[CAPIStatus, CPVuuid]:
+    vds_url = f"{AIXCC_API_HOSTNAME}/submission/vds/"
     response = session.post(
-        f"{AIXCC_API_HOSTNAME}/submission/vds/",
+        vds_url,
         json=vds,
     )
     if not response.ok:
         raise Exception(response.reason, response.status_code)
-    vd_uuid = response.json().get("vd_uuid")
-    status = response.json().get("status")
-
-    return vd_uuid, status
-
-
-def check_vds_status(vd_uuid: VDuuid) -> CAPIStatus:
-    response = session.get(
-        f"{AIXCC_API_HOSTNAME}/submission/vds/{vd_uuid}",
-    )
-    return response.json().get("status")
-
-
-def wait_until_vds_checked(vd_uuid: VDuuid, status: CAPIStatus = "pending", max_retries=15) -> CAPIStatus:
-    retries = 0
-    while status == "pending" and retries < max_retries:
+    content = response.json()
+    vd_uuid = content.get("vd_uuid")
+    while True:
+        response = session.get(
+            f"{vds_url}{vd_uuid}",
+        )
+        if not response.ok:
+            raise Exception(response.reason, response.status_code)
+        content = response.json()
+        status, cpv_uuid = content.get("status"), content.get("cpv_uuid")
+        if not status == "pending":
+            return status, cpv_uuid
         time.sleep(10)  # sleep(10) from run.sh
-        status = check_vds_status(vd_uuid)
-        retries += 1
-    return status
+
+
+def _encode_patch(cpv_uuid: CPVuuid, patch: str) -> GPSubmission:
+    encoded_patch = base64.b64encode(patch.encode()).decode('ascii')
+    return {
+        "cpv_uuid": cpv_uuid,
+        "data": encoded_patch,
+    }
+
+
+def submit_patch(cpv_uuid: CPVuuid, patch: str) -> tuple[CAPIStatus, GPuuid]:
+    gp_url = f"{AIXCC_API_HOSTNAME}/submission/gp/"
+    gp = _encode_patch(cpv_uuid, patch)
+    response = session.post(
+        gp_url,
+        json=gp,
+    )
+    if not response.ok:
+        raise Exception(response.reason, response.status_code)
+    content = response.json()
+    status, gp_uuid = content.get("status"), content.get("gp_uuid")
+    while status == "pending":
+        time.sleep(10)  # sleep(10) from run.sh
+        response = session.get(
+            f"{gp_url}{gp_uuid}",
+        )
+        if not response.ok:
+            raise Exception(response.reason, response.status_code)
+        content = response.json()
+        status, gp_uuid = content.get("status"), content.get("gp_uuid")
+    return status, gp_uuid
