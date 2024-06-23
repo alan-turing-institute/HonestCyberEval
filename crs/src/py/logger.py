b888c088ac6e72b68ef8920a7a63d0757d6ff089
Dan Ristea <dristea@turing.ac.uk>
2024-06-23 19:20:28 +0000

    Add multi-line formatting for logs

--- crs/src/py/logger.py
+++ crs/src/py/logger.py
@@ -7,6 +7,28 @@ import yaml
 from config import AIXCC_CRS_SCRATCH_SPACE
 
 
+class MultiLineFormatter(logging.Formatter):
+    """Multi-line formatter.
+    https://stackoverflow.com/a/66855071
+    """
+
+    def get_header_length(self, record):
+        """Get the header length of a given record."""
+        return len(super().format(logging.LogRecord(
+            name=record.name,
+            level=record.levelno,
+            pathname=record.pathname,
+            lineno=record.lineno,
+            msg='', args=(), exc_info=None
+        )))
+
+    def format(self, record):
+        """Format a record with added indentation."""
+        indent = ' ' * self.get_header_length(record)
+        head, *trailing = super().format(record).splitlines(True)
+        return head + ''.join(indent + line for line in trailing)
+
+
 def filter_maker(level):
     level = getattr(logging, level)
 
